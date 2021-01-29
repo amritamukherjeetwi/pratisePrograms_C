88d8efe3f89f1b4df1c3b07153d1cf49f7ae1d48
Amrita mukherjee <amrita.mukherjee@turjasuzworld.in>
2021-01-29 07:52:35 +0000

    initial commit

---
+++ stringOper1/stringOper1.c
@@ -0,0 +1,12 @@
+//turjasu added this line
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+ char s1[]="my first string";
+ int k = strlen(s1);
+ printf("the string is: %s\n",s1);
+ printf("the string length is: %d\n",k);
+return 0;
+}
